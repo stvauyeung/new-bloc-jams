1f8b603c446e90fed60b38c410d8b9f226a31e6b
Stephen Auyeung <auyeung.chi.kit@gmail.com>
2014-06-18 03:21:52 +0000

    moved sass variables to partials

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -206,11 +206,31 @@ var toggleAlbumView = function() {
   });
 };
 
+var updateSeekPercentage = function($seekBar, event) {
+  var barWidth = $seekBar.width();
+  var offsetX = event.pageX - $seekBar.offset().left;
+
+  var offsetXPercent = (offsetX  / $seekBar.width()) * 100;
+  offsetXPercent = Math.max(0, offsetXPercent);
+  offsetXPercent = Math.min(100, offsetXPercent);
+
+  var percentageString = offsetXPercent + '%';
+  $seekBar.find('.fill').width(percentageString);
+  $seekBar.find('.thumb').css({left: percentageString});
+}
+
+var setupSeekBars = function() {
+  $seekBars = $('.player-bar .seek-bar');
+  $seekBars.click(function(event) {
+    updateSeekPercentage($(this), event);
+  });
+};
 
 if (document.URL.match(/\/album.html/)) {
   $(document).ready(function() {
     changeAlbumView(albumPicasso);
     toggleAlbumView();
+    setupSeekBars();
   });
 };
 });
