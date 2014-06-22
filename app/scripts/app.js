b592d7036a5b7462cd0c92a44b1c259a2c30565c
Stephen Auyeung <auyeung.chi.kit@gmail.com>
2014-06-22 05:44:53 +0000

    added hover to collection

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,6 +2,20 @@
 // require("./collection");
 // require("./album");
 // require("./profile");
+var albumPicasso = {
+  name: 'The Colors',
+  artist: 'Pablo Picasso',
+  label: 'Cubism',
+  year: '1881',
+  albumArtUrl: '/images/album-placeholder.png',
+  songs: [
+    { name: 'Blue', length: '4:26' },
+    { name: 'Green', length: '3:14' },
+    { name: 'Red', length: '5:01' },
+    { name: 'Pink', length: '3:21'},
+    { name: 'Magenta', length: '2:15'}
+  ]
+};
 
 blocJams = angular.module('BlocJams', ['ui.router']);
 
@@ -17,24 +31,36 @@ blocJams.config(['$stateProvider', '$locationProvider', function($stateProvider,
     .state('song', {
       url: '/song',
       templateUrl: '/templates/song.html'
+    })
+    .state('collection', {
+      url: '/collection',
+      controller: 'Collection.controller',
+      templateUrl: '/templates/collection.html'
     });
 }]);
 
-blocJams.controller('Landing.controller', ['$scope', function($scope) {
-  $scope.subText = "Turn the music up!";
-  $scope.subTextClicked = function() {
-    $scope.subText += '!';
-  };
+blocJams
+  .controller('Landing.controller', ['$scope', function($scope) {
+    $scope.subText = "Turn the music up!";
+    $scope.subTextClicked = function() {
+      $scope.subText += '!';
+    };
 
-  $scope.albumURLs = [
-    '/images/album-placeholders/album-1.jpg',
-    '/images/album-placeholders/album-2.jpg',
-    '/images/album-placeholders/album-3.jpg',
-    '/images/album-placeholders/album-4.jpg',
-    '/images/album-placeholders/album-5.jpg',
-    '/images/album-placeholders/album-6.jpg',
-    '/images/album-placeholders/album-7.jpg',
-    '/images/album-placeholders/album-8.jpg',
-    '/images/album-placeholders/album-9.jpg',
-  ];
-}]);
+    $scope.albumURLs = [
+      '/images/album-placeholders/album-1.jpg',
+      '/images/album-placeholders/album-2.jpg',
+      '/images/album-placeholders/album-3.jpg',
+      '/images/album-placeholders/album-4.jpg',
+      '/images/album-placeholders/album-5.jpg',
+      '/images/album-placeholders/album-6.jpg',
+      '/images/album-placeholders/album-7.jpg',
+      '/images/album-placeholders/album-8.jpg',
+      '/images/album-placeholders/album-9.jpg',
+    ];
+  }])
+  .controller('Collection.controller', ['$scope', function($scope) {
+    $scope.albums = [];
+    for (var i = 0; i < 33; i++) {
+      $scope.albums.push(angular.copy(albumPicasso));
+    };
+  }]);
