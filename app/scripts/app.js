6aa7ba7aca6cb0a704b98ae582de81a2fcba640d
Stephen Auyeung <auyeung.chi.kit@gmail.com>
2014-06-29 21:33:07 +0000

    added time indicator for songs

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -110,170 +110,208 @@ blocJams
   }])
   .controller('PlayerBar.controller', ['$scope', 'SongPlayer', 'ConsoleLogger', function($scope, SongPlayer, ConsoleLogger) {
     $scope.songPlayer = SongPlayer;
-    ConsoleLogger.log();
-  }]);
-
-  blocJams
-    .service('SongPlayer', function() {
-      var currentSoundFile = null;
-      var trackIndex = function(album, song) {
-        return album.songs.indexOf(song);
-      };
-
-      return {
-        currentSong: null,
-        currentAlbum: null,
-        playing: false,
-
-        play: function() {
-          this.playing = true;
-          currentSoundFile.play();
-        },
-        pause: function() {
-          this.playing = false;
-          currentSoundFile.pause();
-        },
-        next: function() {
-          if (this.currentSong) {
-            var currentTrackIndex = trackIndex(this.currentAlbum, this.currentSong);
-            currentTrackIndex++;
-            if (currentTrackIndex >= this.currentAlbum.songs.length) {
-              currentTrackIndex = 0;
-            }
-            var song = this.currentAlbum.songs[currentTrackIndex];
-            this.setSong(this.currentAlbum, song);
-          };
-        },
-        previous: function() {
-          if (this.currentSong) {
-            var currentTrackIndex = trackIndex(this.currentAlbum, this.currentSong);
-            currentTrackIndex--;
-            if (currentTrackIndex < 0) {
-              currentTrackIndex = this.currentAlbum.songs.length - 1;
-            }
-            var song = this.currentAlbum.songs[currentTrackIndex];
-            this.setSong(this.currentAlbum, song);
-          };
-        },
-        seek: function(time) {
-          if (currentSoundFile) {
-            currentSoundFile.setTime(time);
-          }
-        },
-        setSong: function(album, song) {
-          if (currentSoundFile) {
-            currentSoundFile.stop();
-          }
-          this.currentAlbum = album,
-          this.currentSong = song;
-          currentSoundFile = new buzz.sound(song.audioUrl, {
-            formats: ["mp3"],
-            preload: true
-          });
-
-          this.play();
-        }
-      };
-    })
-    .service('ConsoleLogger', function() {
-      return {
-        logString: '',
-
-        setString: function(newString) {
-          this.logString = newString;
-        },
-        log: function() {
-          console.log(this.logString);
-        }
-      };
+    
+    SongPlayer.onTimeUpdate(function(event, time) {
+      $scope.$apply(function() {
+        $scope.playTime = time;
+      });
     });
-
-  blocJams.directive('slider', ['$document', function() {
-
-    var calculateSliderPercentFromMouseEvent = function($slider, event) {
-      var offsetX = event.pageX - $slider.offset().left;
-      var sliderWidth = $slider.width();
-      var offsetXPercent = ( offsetX / sliderWidth );
-      offsetXPercent = Math.max(0, offsetXPercent);
-      offsetXPercent = Math.min(1, offsetXPercent);
-      return offsetXPercent;
-    };
-
-    var numberFromValue = function(value, defaultValue) {
-      if (typeof value === 'number') {
-        return value;
-      }
-
-      if (typeof value === 'undefined') {
-        return defaultValue;
-      }
-
-      if (typeof value === 'string') {
-        return Number(value);
-      }
-    };
-   
-    return {
-      templateUrl: 'templates/directives/slider.html',
-      replace: true,
-      restrict: 'E',
-      scope: {
-        onChange: '&'
-      },
-      link: function(scope, element, attributes) {
-        scope.value = 0;
-        scope.max = 100;
-        var $seekBar = $(element);
-        console.log(attributes);
-        
-        attributes.$observe('value', function(newValue) {
-          scope.value = numberFromValue(newValue, 0);
-        });
-
-        attributes.$observe('max', function(newValue) {
-          scope.max = numberFromValue(newValue, 100) || 100;
-        });
-        
-        var percentString = function() {
-          var value = scope.value || 0;
-          var max = scope.max || 100;
-          percent = value / max * 100;
-          return percent + "%";
-        };
-
-        var notifyCallback = function(newValue) {
-          if (typeof scope.onChange === 'function') {
-            scope.onChange({value: newValue});
-          };
-        };
-
-        scope.fillStyle = function() {
-          return {width: percentString()};
-        };
-
-        scope.thumbStyle = function() {
-          return {left: percentString()};
-        };
-
-        scope.onClickSlider = function(event) {
-          var percent = calculateSliderPercentFromMouseEvent($seekBar, event);
-          scope.value = percent * scope.max;
-          notifyCallback(scope.value);
-        };
-
-        scope.trackThumb = function() {
-          $document.bind('mousemove.thumb', function(event) {
-            scope.$apply(function() {
-              scope.value = percent * scope.max;
-              notifyCallback(scope.value);
-            });
-          });
-
-          $document.bind('mouseup.thumb', function() {
-            $document.unbind('mousemove.thumb');
-            $document.unbind('mouseup.thumb');
-          });
-        };
-      }
-    };
   }]);
+
+blocJams
+  .service('SongPlayer', ['$rootScope', function($rootScope) {
+    var currentSoundFile = null;
+    var trackIndex = function(album, song) {
+      return album.songs.indexOf(song);
+    };
+
+    return {
+      currentSong: null,
+      currentAlbum: null,
+      playing: false,
+
+      play: function() {
+        this.playing = true;
+        currentSoundFile.play();
+      },
+      pause: function() {
+        this.playing = false;
+        currentSoundFile.pause();
+      },
+      next: function() {
+        if (this.currentSong) {
+          var currentTrackIndex = trackIndex(this.currentAlbum, this.currentSong);
+          currentTrackIndex++;
+          if (currentTrackIndex >= this.currentAlbum.songs.length) {
+            currentTrackIndex = 0;
+          }
+          var song = this.currentAlbum.songs[currentTrackIndex];
+          this.setSong(this.currentAlbum, song);
+        };
+      },
+      previous: function() {
+        if (this.currentSong) {
+          var currentTrackIndex = trackIndex(this.currentAlbum, this.currentSong);
+          currentTrackIndex--;
+          if (currentTrackIndex < 0) {
+            currentTrackIndex = this.currentAlbum.songs.length - 1;
+          }
+          var song = this.currentAlbum.songs[currentTrackIndex];
+          this.setSong(this.currentAlbum, song);
+        };
+      },
+      seek: function(time) {
+        if (currentSoundFile) {
+          currentSoundFile.setTime(time);
+        }
+      },
+      onTimeUpdate: function(callback) {
+        return $rootScope.$on('sound:timeupdate', callback);
+      },
+      setSong: function(album, song) {
+        if (currentSoundFile) {
+          currentSoundFile.stop();
+        }
+        this.currentAlbum = album;
+        this.currentSong = song;
+
+        currentSoundFile = new buzz.sound(song.audioUrl, {
+          formats: ["mp3"],
+          preload: true
+        });
+
+        currentSoundFile.bind('timeupdate', function(e) {
+          $rootScope.$broadcast('sound:timeupdate', this.getTime());
+        });
+
+        this.play();
+      }
+    };
+  }])
+  .service('ConsoleLogger', function() {
+    return {
+      logString: '',
+
+      setString: function(newString) {
+        this.logString = newString;
+      },
+      log: function() {
+        console.log(this.logString);
+      }
+    };
+  });
+
+blocJams.directive('slider', ['$document', function() {
+
+  var calculateSliderPercentFromMouseEvent = function($slider, event) {
+    var offsetX = event.pageX - $slider.offset().left;
+    var sliderWidth = $slider.width();
+    var offsetXPercent = ( offsetX / sliderWidth );
+    offsetXPercent = Math.max(0, offsetXPercent);
+    offsetXPercent = Math.min(1, offsetXPercent);
+    return offsetXPercent;
+  };
+
+  var numberFromValue = function(value, defaultValue) {
+    if (typeof value === 'number') {
+      return value;
+    }
+
+    if (typeof value === 'undefined') {
+      return defaultValue;
+    }
+
+    if (typeof value === 'string') {
+      return Number(value);
+    }
+  };
+ 
+  return {
+    templateUrl: 'templates/directives/slider.html',
+    replace: true,
+    restrict: 'E',
+    scope: {
+      onChange: '&'
+    },
+    link: function(scope, element, attributes) {
+      scope.value = 0;
+      scope.max = 100;
+      var $seekBar = $(element);
+      console.log(attributes);
+      
+      attributes.$observe('value', function(newValue) {
+        scope.value = numberFromValue(newValue, 0);
+      });
+
+      attributes.$observe('max', function(newValue) {
+        scope.max = numberFromValue(newValue, 100) || 100;
+      });
+      
+      var percentString = function() {
+        var value = scope.value || 0;
+        var max = scope.max || 100;
+        percent = value / max * 100;
+        return percent + "%";
+      };
+
+      var notifyCallback = function(newValue) {
+        if (typeof scope.onChange === 'function') {
+          scope.onChange({value: newValue});
+        };
+      };
+
+      scope.fillStyle = function() {
+        return {width: percentString()};
+      };
+
+      scope.thumbStyle = function() {
+        return {left: percentString()};
+      };
+
+      scope.onClickSlider = function(event) {
+        var percent = calculateSliderPercentFromMouseEvent($seekBar, event);
+        scope.value = percent * scope.max;
+        notifyCallback(scope.value);
+      };
+
+      scope.trackThumb = function() {
+        $document.bind('mousemove.thumb', function(event) {
+          scope.$apply(function() {
+            scope.value = percent * scope.max;
+            notifyCallback(scope.value);
+          });
+        });
+
+        $document.bind('mouseup.thumb', function() {
+          $document.unbind('mousemove.thumb');
+          $document.unbind('mouseup.thumb');
+        });
+      };
+    }
+  };
+}]);
+
+blocJams.filter('timecode', function() {
+  return function(seconds) {
+    seconds = Number.parseFloat(seconds);
+
+    if (Number.isNaN(seconds)) {
+      return '-:--';
+    }
+
+    var wholeSeconds = Math.floor(seconds);
+    var minutes = Math.floor(wholeSeconds / 60);
+
+    remainingSeconds = wholeSeconds % 60;
+
+    var output = minutes + ':';
+
+    if (remainingSeconds < 10) {
+      output += '0';
+    }
+
+    output += remainingSeconds;
+
+    return output;
+  }
+});
